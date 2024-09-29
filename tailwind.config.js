5f1f07cfc8cd08ae23a3459b790c44940a31ae88
Khokon Chandra <khokonchandra4@gmail.com>
2024-09-30 00:06:17 +0600

    project initialized and layout implemented

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode:'jit',
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  
+  theme: {
+    extend: {
+      
+    },
+  },
+  plugins: [
+      require('flowbite/plugin')
+  ],
+}
