b93aad1eeb0ce567ba78a0e522097a4d09db17ec
Khokon-Chandra <khokonchandra4@gmail.com>
2024-10-29 16:30:39 +0600

    description updated

--- resources/js/Pages/Problem/Pane/Description.vue
+++ resources/js/Pages/Problem/Pane/Description.vue
@@ -1,6 +1,10 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
+const problem = usePage().props.problem;
+
+const isTagCollapse = ref(false);
 
 </script>
 
@@ -8,93 +12,107 @@ import { Link } from '@inertiajs/vue3';
     <div class="h-full grid grid-rows-[auto_1fr] rounded-lg bg-white dark:bg-neutral-800 shadow-sm">
 
         <div class="dark:bg-neutral-700/50 flex p-1">
-            <Link preserve-state="true" :href="route('problems.show', 'two-sum')"
+            <Link preserve-state="true" :href="route('problems.show', problem.slug)"
                 class="px-2 py-1 rounded-md flex justify-between items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-700">
             <span class="text-blue-400">
-                <font-awesome-icon icon="note-sticky"/>
+                <font-awesome-icon icon="note-sticky" />
             </span>
             <span class="text-sm text-neutral-600 dark:text-neutral-200">Description</span>
             </Link>
 
-            <Link preserve-state="true" :href="route('problems.submissions','two-sum')"
+            <Link preserve-state="true" :href="route('problems.submissions', problem.slug)"
                 class="px-2 py-1 rounded-md flex justify-between items-center gap-2 hover:bg-neutral-200 dark:hover:bg-neutral-700">
             <span class="text-blue-400">
-                <font-awesome-icon icon="note-sticky"/>
+                <font-awesome-icon icon="note-sticky" />
             </span>
             <span class="text-sm text-neutral-600 dark:text-neutral-200">Submissions</span>
             </Link>
         </div>
 
-        <div class="p-4 overflow-y-auto">
+        <div class="h-screen p-4 overflow-y-auto  custom-scrollbar">
             <!-- Title -->
             <h1 class="text-2xl font-bold space-x-2 text-neutral-600 dark:text-neutral-200 mb-3">
-                <span>1.</span>
-                <span>Two Sum</span>
+                <span>{{ problem.id }}.</span>
+                <span class="capitalize text-wrap">{{ problem.title }}</span>
             </h1>
             <!-- Topics tags -->
             <div class="flex gap-2 mb-3">
+                <span :class="{
+                    ' text-green-500': problem.difficulty == 'easy',
+                    ' text-yellow-500': problem.difficulty == 'medium',
+                    ' text-red-600': problem.difficulty == 'hard',
+                }" class="text-sm font-thin bg-neutral-200 dark:bg-neutral-700/50 rounded-full px-2 capitalize">{{
+                    problem.difficulty }}</span>
                 <span
-                    class="text-sm font-thin text-teal-600 bg-neutral-200 dark:bg-neutral-700 rounded-full px-2">Easy</span>
-                <span
-                    class="text-sm font-thin text-slate-600 dark:text-slate-200 bg-neutral-200 dark:bg-neutral-700 rounded-full px-2">Topics</span>
+                    class="text-sm font-thin text-slate-600 dark:text-slate-300 bg-neutral-200 dark:bg-neutral-700/50 rounded-full px-2 capitalize">{{
+                        problem.topic.name }}</span>
             </div>
             <!-- Description -->
             <div class="mb-10">
-                <p class="text-neutral-600 dark:text-neutral-200">Given an array of integers nums and an integer
-                    target, return indices of the two numbers
-                    such that they add up to target.
-
-                    You may assume that each input would have exactly one solution, and you may not use the
-                    same element twice.
-
-                    You can return the answer in any order.</p>
+                <p class="text-neutral-600 dark:text-neutral-200 text-wrap">{{ problem.description }}</p>
             </div>
 
             <!-- Example 1-->
-            <div class="mb-4">
-                <h5 class="text-lg text-bold text-neutral-600 dark:text-neutral-200 mb-3">Example 1.</h5>
+            <div class="mb-4" v-for="(example, index) in problem.examples" :key="index">
+                <h5 class="text-lg text-bold text-neutral-600 dark:text-neutral-200 mb-3">Example {{ index + 1 }}.</h5>
                 <div class="border-l border-neutral-300 dark:border-neutral-500 px-3">
                     <!-- Input -->
                     <div class="flex gap-4">
                         <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-200">Input:</h6>
-                        <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-400">nums = [2,7,11,15],
-                            target = 9</h6>
+                        <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-400">{{ example.input }}</h6>
                     </div>
                     <!-- Output -->
                     <div class="flex gap-4">
                         <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-200">Output:</h6>
-                        <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-400">[0,1]</h6>
+                        <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-400">{{ example.output }}</h6>
                     </div>
                 </div>
             </div>
 
-
-            <!-- Example 2-->
-            <div class="mb-4">
-                <h5 class="text-lg text-bold text-neutral-600 dark:text-neutral-200 mb-3">Example 2.</h5>
-                <div class="border-l border-neutral-300 dark:border-neutral-500 px-3">
-                    <!-- Input -->
-                    <div class="flex gap-4">
-                        <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-200">Input:</h6>
-                        <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-400">nums = [2,7,11,15],
-                            target = 9</h6>
-                    </div>
-                    <!-- Output -->
-                    <div class="flex gap-4">
-                        <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-200">Output:</h6>
-                        <h6 class="text-lg text-bold text-neutral-600 dark:text-neutral-400">[0,1]</h6>
-                    </div>
-                </div>
-            </div>
 
             <!-- Constraints -->
             <div class="mb-8 mt-10">
                 <h5 class="text-lg text-bold text-neutral-600 dark:text-neutral-200 mb-3">Constraints:</h5>
-                <ul class="list-disc pl-10">
-                    <li class=" text-neutral-500">2 <= nums.length <=104</li>
-                    <li class=" text-neutral-500">-109 <= nums[i] <=109</li>
+                <ul class="list-disc pl-10" v-for="(constraint, index) in problem.constraints" :key="index">
+                    <li class=" text-neutral-500">{{ constraint.value }}</li>
                 </ul>
             </div>
+
+
+            <div class="bg-neutral-100 dark:bg-neutral-700/20 rounded-lg">
+                <div @click="isTagCollapse = ! isTagCollapse" class="cursor-pointer p-4 flex justify-between items-center ">
+                    <h4 class="text-lg text-bold text-neutral-600 dark:text-neutral-200">Tags</h4>
+                    <font-awesome-icon class="text-neutral-600 dark:text-neutral-200" icon="angle-down" />
+                </div>
+                <div v-if="isTagCollapse" class="flex gap-4 flex-wrap p-4">
+                    <span v-for="tag in problem.tags.slice(0, 20)" :key="tag.id"
+                        class="text-sm font-thin bg-neutral-200 dark:bg-neutral-700/50 rounded-full px-2 capitalize text-neutral-500">
+                        {{ tag.name }}
+                    </span>
+                </div>
+            </div>
+
+
+            <div class="h-48"></div>
+
         </div>
     </div>
 </template>
+<style scoped>
+/* Custom scrollbar styles */
+.custom-scrollbar::-webkit-scrollbar {
+    width: 5px;
+    /* Set the width of the scrollbar */
+}
+
+.custom-scrollbar::-webkit-scrollbar-thumb {
+    background-color: #73706c;
+    /* Tailwind `neutral-500` (Scrollbar thumb color) */
+    border-radius: 8px;
+}
+
+.custom-scrollbar::-webkit-scrollbar-track {
+    background-color: #524f4c;
+    /* Tailwind `neutral-700` (Scrollbar track color) */
+}
+</style>
